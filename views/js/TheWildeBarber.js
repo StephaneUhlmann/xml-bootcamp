b5536e029104d5cf486893b68cf8db0b534c04ff
StephaneUhlmann <srodrigues.farma@outlook.com>
2020-12-30 13:26:51 +0000

    show/hide function

--- views/js/TheWildeBarber.js
+++ views/js/TheWildeBarber.js
@@ -31,3 +31,12 @@ function getParentTag(oNode, sParentType) {
     };
     return oParent;
 };
+
+function myFunction() {
+  var x = document.getElementById("myDIV");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
