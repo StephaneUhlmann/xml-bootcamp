2b0cae0f967f1e0312d95e51b6a164d3fd56179f
StephaneUhlmann <srodrigues.farma@outlook.com>
2021-04-05 14:31:27 +0000

    Adding models and controllers

---
+++ index.js
@@ -0,0 +1,24 @@
+const http = require('http'),
+axios = require('axios'),
+logger = require('morgan'),
+cors = require('cors'),
+express = require('express'),
+bodyParser = require('body-parser'),
+mongoose = require('mongoose');
+
+var app = express();
+var port = 8000;
+
+app.use(bodyParser.json());
+app.use(logger('tiny'));
+app.use(require('./routes'));
+
+app.listen(port, function(err){
+    console.log('Listening on port: ' + port);
+});
+
+const dbURI = "mongodb://localhost/test";
+
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+        .then((result) => console.log('connected to db'))
+        .catch((err) => console.log(err));
