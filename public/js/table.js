ec6920baec3af34b2275f420f46022d8ae904895
StephaneUhlmann <srodrigues.farma@outlook.com>
2021-05-02 15:13:58 +0000

    delete row fix

--- public/js/table.js
+++ public/js/table.js
@@ -1,47 +1,47 @@
 //https://simonplend.com/how-to-use-fetch-to-post-form-data-as-json-to-your-api/
 //POST method to add barber users to the database
-        async function postFormDataAsJson({ url, formData }) {
-            const plainFormData = Object.fromEntries(formData.entries());
-            const formDataJsonString = JSON.stringify(plainFormData);
+async function postFormDataAsJson({ url, formData }) {
+    const plainFormData = Object.fromEntries(formData.entries());
+    const formDataJsonString = JSON.stringify(plainFormData);
 
-            const fetchOptions = {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                    Accept: "application/json",
-                },
-                body: formDataJsonString,
-            };
+    const fetchOptions = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+        body: formDataJsonString,
+    };
 
-            const response = await fetch(url, fetchOptions);
+    const response = await fetch(url, fetchOptions);
 
-            if (!response.ok) {
-                const errorMessage = await response.text();
-                throw new Error(errorMessage);
-            }
+    if (!response.ok) {
+        const errorMessage = await response.text();
+        throw new Error(errorMessage);
+    }
 
-            return response.json();
-        }
+    return response.json();
+}
 
-        async function handleFormSubmit(event) {
-            event.preventDefault();
+async function handleFormSubmit(event) {
+    event.preventDefault();
 
-            const form = event.currentTarget;
-            const url = form.action;
+    const form = event.currentTarget;
+    const url = form.action;
 
-            try {
-                const formData = new FormData(form);
-                const responseData = await postFormDataAsJson({ url, formData });
+    try {
+        const formData = new FormData(form);
+        const responseData = await postFormDataAsJson({ url, formData });
 
-                console.log({ responseData });
-            } catch (error) {
-                console.error(error);
-            }
-        }
+        console.log({ responseData });
+    } catch (error) {
+        console.error(error);
+    }
+}
 
-        const elForm = document.getElementById("form");
-        elForm.addEventListener("submit", handleFormSubmit);
-        
+const elForm = document.getElementById("form");
+elForm.addEventListener("submit", handleFormSubmit);
+
 
 //https://howtocreateapps.com/fetch-and-display-json-html-javascript/
 //GET all data from the colection services 
@@ -60,27 +60,30 @@ fetch('/services')
 //Method that recieves the data and creates a table in the frontend    
 function appendData(data) {
     document.getElementById('table-rows').innerHTML = data.map((item) => {
-        return '<tr name=' + item._id + '><td>' + item.barbername + '</td><td>' + item.service + '</td><td>' + item.price + '</td><td><input type="button" value="X" onclick="DeleteRowFunction(this);"></td></tr>';
+        return '<tr><td>' + item.barbername + '</td><td>' + item.service + '</td><td>' + item.price + '</td><td><input id=' + item._id + ' type="button" value="X" onclick="deleteRow(this)" onClick="window.location.reload();"></td></tr>';
     }).join('');
 }
 
 //https://stackoverflow.com/questions/11553768/remove-table-row-after-clicking-table-row-delete-button
 //Method for deleting a row
+/*
 function DeleteRowFunction(btndel) {
-    /* axios.delete('services/'+ id)
-        .then(res => {
-            console.log(response);
-        }) */
+    fetch('/services/' + btndel.getElementById("key"), { method: 'DELETE' })
+        .then(() => this.setState({ status: 'Delete successful' }));
 
     if (typeof (btndel) == "object") {
         $(btndel).closest("tr").remove();
-     var id = document.getElementById("key").nodeValue;
-     //   let id = $(btndel).closest("key");
-      //  var id = btndel.target.closest("[key]").getAttribute("key");
-        fetch('/services/' + id, { method: 'DELETE' })
-            .then(() => this.setState({ status: 'Delete successful' }));
-
     } else {
         return false;
     }
 }
+*/
+
+//https://stackoverflow.com/questions/3623110/get-an-elements-id
+function deleteRow(btn) {
+  fetch('/services/' + btn.id, { method: 'DELETE' })
+        .then(() => this.setState({ status: 'Delete successful' }));
+}
+
+
+
